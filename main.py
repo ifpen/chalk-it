ac3fb692c1f857d5ebce377a43a5ed12a7cb86fd
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2023-11-28 15:45:35 +0100

    fix: server flask #89

--- main.py
+++ main.py
@@ -2,7 +2,17 @@
 # shoud import server.py ....
 # should handle commande line arguements
 
-from back_end.app.server import Main
+import os
+
+current_directory = os.getcwd()
+directory_name = os.path.basename(current_directory)
+
+if directory_name == 'chlkt':
+    # Poduction mode
+    from app.server import Main
+else:
+    # Developpe mode
+    from back_end.app.server import Main
 
 if __name__ == "__main__":
     Main.main()
