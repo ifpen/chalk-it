78e4d687e0363eb5e908bc14d44f6a8cebea4ec9
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-01-24 10:15:01 +0100

    testing plotly

--- front-end/taipy_page.py
+++ front-end/taipy_page.py
@@ -1,9 +1,14 @@
 from taipy.gui.custom import Page
 from source.connectors.taipy.resource_handler import PureHTMLResourceHandler
+import plotly.express as px
 
 a = 8
 b = 10
 c = a + b * 2
+
+
+df = px.data.gapminder().query("country=='Canada'")
+fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
 
 def on_change(state):
     state.c = state.a + state.b * 2
