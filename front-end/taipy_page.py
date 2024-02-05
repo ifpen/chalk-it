85775418c41b8b8edf8edaffff05c9e84acd178d
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-02-05 21:05:16 +0100

    example rename, config build prod

--- front-end/taipy_page.py
+++ front-end/taipy_page.py
@@ -5,10 +5,8 @@ import plotly.express as px
 a = 8
 b = 10
 c = a + b * 2
-d = 86
 
 
-f = 4
 
 df = px.data.gapminder().query("country=='Canada'")
 fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
@@ -27,8 +25,6 @@ selectd_count = 40
 def on_change(state, var, val):
     if ((var == 'a') or (var == 'b')):
         state.c = state.a + state.b * 2
-    if var == 'selectd_fruit':
-        state.selectd_count = state.counts[state.fruits.index(val)]
 
 # Create a Page instance with the resource handler
 page = Page(PureHTMLResourceHandler())
