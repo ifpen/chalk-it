9c5007bdc832f4a89a85971cba97c3744a3b7ef0
Abir EL FEKI <31621762+abirEF@users.noreply.github.com>
2024-02-07 17:49:12 +0100

    modif for timer need

--- front-end/source/kernel/datanodes/execution-engine/DatanodeScheduler.js
+++ front-end/source/kernel/datanodes/execution-engine/DatanodeScheduler.js
@@ -159,7 +159,7 @@ function DatanodeScheduler(datanodesDependency, startNodes, triggeredNodes, init
               bForceAutoStart = isForceAutoStart(op); // temp use
               datanodesManager
                 .getDataNodeByName(op)
-                .updateNow(bCalledFromOrchestrator, bForceAutoStart, bAllPredExecuted);
+                .updateNow(bCalledFromOrchestrator, bForceAutoStart, bAllPredExecuted, callOrigin);
             } else {
               operationsToExecute.delete(op);
               operationsNotReady.add(op);
@@ -192,7 +192,7 @@ function DatanodeScheduler(datanodesDependency, startNodes, triggeredNodes, init
 
                   datanodesManager
                     .getDataNodeByName(op)
-                    .updateNow(bCalledFromOrchestrator, bForceAutoStart, bAllPredExecuted);
+                    .updateNow(bCalledFromOrchestrator, bForceAutoStart, bAllPredExecuted, callOrigin);
                 } else {
                   if (!offSchedLogUser && !xDashConfig.disableSchedulerLog)
                     console.log('operation ' + op + ' is ready but cannot be executed because of its predecessors.');
