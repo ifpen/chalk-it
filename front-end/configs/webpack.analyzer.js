1a15dd5adb0465216d91e6f24f55628f888c9a2f
BARTEMENT Tristan <tristan.bartement@ifpen.fr>
2024-02-16 18:50:08 +0100

    Setup webpack-bundle-analyzer

---
+++ front-end/configs/webpack.analyzer.js
@@ -0,0 +1,8 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.prod.js');
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = merge(common, {
+  plugins: [new BundleAnalyzerPlugin()],
+});
