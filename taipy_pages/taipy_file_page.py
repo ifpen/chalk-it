7e89c43a4229033926e5d612021c228933957009
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-02-29 13:11:04 +0100

    feat: add upload_file function

--- taipy_pages/taipy_file_page.py
+++ taipy_pages/taipy_file_page.py
@@ -4,13 +4,15 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 from back_end.taipy.resource_handler import PureHTMLResourceHandler
 from back_end.taipy.chalkit_manager import *
-
 from taipy.gui.custom import Page
 
 a = 8
 b = 10
 c = a + b * 2
 
+# Called from a file loader widgets
+def upload_file(state, name, payload):
+    print('file data', payload["file_data"])
 
 def on_change(state, var, val):
     state.c = state.a + state.b * 2
