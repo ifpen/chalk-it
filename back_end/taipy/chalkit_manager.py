203920649e512e276bdaf4a005557ee41b7df820
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-01 10:03:57 +0100

    style: cleanup

--- back_end/taipy/chalkit_manager.py
+++ back_end/taipy/chalkit_manager.py
@@ -1,7 +1,5 @@
 from .function_json_adapter import FunctionJsonAdapter
-from .resource_handler import PureHTMLResourceHandler
 from pathlib import Path
-from taipy.gui.custom import Page
 import json
 import os
 import sys
@@ -15,10 +13,10 @@ json_data = ""
 has_file_saved = False
 file_list = {}
 
-def load_file(state):
+def load_file(state, action_name):
     state.json_data = Path(state.file_name).read_text()
 
-def save_file(state, name, payload):
+def save_file(state, action_name, payload):
     if "data" not in payload:
         return
     with open(state.file_name, "w") as f:
@@ -26,14 +24,14 @@ def save_file(state, name, payload):
         state.has_file_saved = True
 
 # file should only be within base path
-def select_file(state, name, payload):
+def select_file(state, action_name, payload):
     if "file_name" not in payload:
         return
     potential_file_path = base_path / payload['file_name']
     if potential_file_path.exists():
         state.file_name = str(potential_file_path)
         
-def get_file_list(state, name):
+def get_file_list(state, action_name):
     # Use glob to find .xprjson files directly within the base directory.
     file_names = [file.name for file in base_path.glob('*.xprjson')]
     file_list_obj = {
