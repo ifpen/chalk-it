dfc168fe69084160a54c9473a33d22b513c4dce8
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-01 12:17:11 +0100

    fixes

--- taipy_pages/iris_demo_page.py
+++ taipy_pages/iris_demo_page.py
@@ -2,8 +2,8 @@ import sys
 from pathlib import Path
 # Add the parent directory of `back_end` to sys.path
 sys.path.append(str(Path(__file__).resolve().parent.parent))
-from back_end.taipylink.resource_handler import PureHTMLResourceHandler
-from back_end.taipylink.chalkit_manager import *
+from back_end.taipy.resource_handler import PureHTMLResourceHandler
+from back_end.taipy.chalkit_manager import *
 
 import pandas as pd
 from sklearn import datasets
