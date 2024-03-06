c196287e92902d09a0490874b4384fea85814034
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-06 23:57:59 +0100

    working velib example

--- taipy_pages/velib_real_time_demo.py
+++ taipy_pages/velib_real_time_demo.py
@@ -4,4 +4,4 @@ from velib_real_time_page import page
 gui = Gui()
 gui.add_page("velib-real-time", page)
 
-gui.run(run_browser=True, use_reloader=False)
+gui.run(run_browser=True, use_reloader=True)
--- taipy_pages/velib_real_time_page.py
+++ taipy_pages/velib_real_time_page.py
@@ -30,15 +30,20 @@ def process_stations_status(data):
     return df_rt
 
 def augment_with_geodata(df_rt, selected_polygon_geojson=None):
+
     dfg = df_rt.copy()
     dfg['bikes_availability_rate'] = dfg['numbikesavailable'] / dfg['capacity']
     dfg['station_full'] = (dfg['numbikesavailable'] == dfg['capacity']).astype(int)
     dfg['station_empty'] = (dfg['numbikesavailable'] == 0).astype(int)
     geometry = [Point(xy) for xy in zip(dfg['lng'], dfg['lat'])]
     geo_df = gpd.GeoDataFrame(dfg, geometry=geometry)
-    if selected_polygon_geojson is not None:
-        selected_polygon = shape(selected_polygon_geojson)
+    if selected_polygon_geojson is not None:   
+        selected_gpd = gpd.GeoDataFrame.from_features(selected_polygon_geojson)
+        # Assume selected_gpd is a single polygon for simplicity
+        selected_polygon = selected_gpd.iloc[0]['geometry']
+        # Create a boolean mask where each value is True if the point is within the selected geometry
         mask = geo_df.within(selected_polygon)
+        # Use the mask to filter the GeoDataFrame
         filtered_geo_df = geo_df[mask]
         return filtered_geo_df
     else:
@@ -59,7 +64,7 @@ def generate_echarts_bar_graph(data, dockBar):
     }
     return echarts_option
 
-def generate_heatmap_data(geo_df):
+def generate_heatmap_data(geo_df, cfg):
     """
     Generate heatmap data from a GeoDataFrame.
 
@@ -67,7 +72,7 @@ def generate_heatmap_data(geo_df):
     - geo_df: A GeoDataFrame containing bike station data with latitude, longitude, and bikes availability rate.
 
     Returns:
-    - A dictionary suitable for generating a heatmap on the JavaScript side, including the location and bikes availability rate.
+    - A dictionary suitable for generating a heatmap, including the location and bikes availability rate.
     """
     # Extract relevant data for the heatmap
     heatmap_data = geo_df[['lat', 'lng', 'bikes_availability_rate']].copy()
@@ -78,25 +83,56 @@ def generate_heatmap_data(geo_df):
     # Prepare the configuration for the heatmap display
     heatmap_config = {
         "data": heatmap_data_list,
-        "config": {
-            "opacity": 0.9,
-            "radius": 80,
-            "disableAutoscale": False,
-            "min": 0,
-            "max": 1,
-            "colorScale": "interpolateRdYlBu",
-            "reverseColorScale": False
-        }
+        "config": cfg
     }
     return heatmap_config
 
+def compute_global_stats(dfg):
+    return {
+    "Number of available bikes": "{:,}".format(int(dfg['numbikesavailable'].sum())).replace(',', ' '),
+    "Number of available docks": "{:,}".format(int(dfg['numdocksavailable'].sum())).replace(',', ' '),
+    "Capacity" : "{:,}".format(int(dfg['capacity'].sum())).replace(',', ' '),
+    "Number of full stations": "{:,}".format(int(dfg['station_full'].sum())).replace(',', ' '),
+    "Number of empty stations": "{:,}".format(int(dfg['station_empty'].sum())).replace(',', ' '),
+    "Number of renting stations": "{:,}".format(int(dfg['is_renting'].sum())).replace(',', ' '),
+    "Max capacity" : int(dfg['capacity'].max())
+
+    }
+
 # Example of incorporating the function into the workflow
+cfg ={
+    "opacity": 0.9,
+    "radius": 80,
+    "disableAutoscale": False,
+    "min": 0,
+    "max": 1,
+    "colorScale": "interpolateRdYlBu",
+    "reverseColorScale": False
+}
+
 stations_status_json = fetch_stations_status()
 df_rt = process_stations_status(stations_status_json)
 dfg = augment_with_geodata(df_rt)
-heatmap_data_config = generate_heatmap_data(dfg)
+heatmap_json = generate_heatmap_data(dfg, cfg)
 echarts_option_json = generate_echarts_bar_graph(dfg['numbikesavailable'], dockBar=False)
+global_stats = compute_global_stats(dfg)
 
+selected_polygon_geojson = None
+dockBar = False
 
+def on_change(state, var, val):
+    if var == "selected_polygon_geojson":
+        selected_polygon_geojson = val
+        dfg = augment_with_geodata(state.df_rt, selected_polygon_geojson)
+        heatmap_json = generate_heatmap_data(dfg, state.cfg)
+        echarts_option_json = generate_echarts_bar_graph(dfg['numbikesavailable'], state.dockBar)
+        state.heatmap_json = heatmap_json
+        state.echarts_option_json = echarts_option_json
+        state.global_stats = compute_global_stats(dfg)
+
+    if var == "cfg":
+        cfg = val
+        state.heatmap_json = generate_heatmap_data(state.dfg, cfg)
+    
 # Create a Page instance with the resource handler
 page = Page(PureHTMLResourceHandler())
