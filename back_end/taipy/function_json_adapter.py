7e461afd292de3e2b7f0d4354e19974ef4783dd6
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-06 15:23:40 +0100

    improving serializers

--- back_end/taipy/function_json_adapter.py
+++ back_end/taipy/function_json_adapter.py
@@ -1,45 +1,43 @@
 from taipy.gui import JsonAdapter
 from types import FunctionType
-from pathlib import Path
 import json
 import io, base64
-import math
-import datetime
+import numpy as np
+
+class CustomEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, float) and np.isnan(obj):
+            return None
+        return json.JSONEncoder.default(self, obj)
 
 class FunctionJsonAdapter(JsonAdapter):
+    @staticmethod
+    def json_fix(obj):
+        jsn_str = json.dumps(obj, cls=CustomEncoder)
+        jsn_obj = json.loads(jsn_str)  # Corrected from json.parse to json.loads
+        return jsn_obj
+        
     def parse(self, o):
         if isinstance(o, FunctionType):
             return o.__name__
-        if "plotly.graph_objs._figure.Figure" in str(type(o)):
+        elif "plotly.graph_objs._figure.Figure" in str(type(o)):
             return json.loads(o.to_json(validate=True, pretty=False))
-        #if "matplotlib.figure.Figure" in str(type(o)): # seems impossible in other thread than main thread
-        #    buf = io.BytesIO()
-        #    o.savefig(buf, format='png')
-        #    buf.seek(0)
-        #    img_str = base64.b64encode(buf.read()).decode('UTF-8')    
-        #    return img_str
-        if "pandas.core.frame.DataFrame" in str(type(o)):
-            return json.loads(o.to_json(orient='split'))
-        if "folium.folium.Map" in str(type(o)):
+        elif "pandas.core.frame.DataFrame" in str(type(o)):
+            return self.json_fix(json.loads(o.to_json(orient='split')))
+        elif "folium.folium.Map" in str(type(o)):
             return o._repr_html_()
-        if "geopandas.geodataframe.GeoDataFrame" in str(type(o)):
-            return json.loads(o.to_json())
-        if "PIL.Image.Image" in str(type(o)):
+        elif "geopandas.geodataframe.GeoDataFrame" in str(type(o)):
+            return self.json_fix(json.loads(o.to_json()))
+        elif "PIL.Image.Image" in str(type(o)):
             return self._image_to_base64(o)
-        
-    # Function to convert an image to base64
+        else:
+            return self.json_fix(super().parse(o))  # Corrected super().parse(self, o) to super().parse(o)
+
+    @staticmethod
     def _image_to_base64(image):
-        # Create a BytesIO buffer to save the image
         buffered = io.BytesIO()
-        # Save the image to the buffer in JPEG format
         image.save(buffered, format="JPEG")
-        # Get the byte data from the buffer
         img_byte = buffered.getvalue()
-        # Encode the bytes to base64
         img_base64 = base64.b64encode(img_byte)
-        # Convert bytes to string for HTML use
         img_base64_str = img_base64.decode('utf-8')
-        html_img_str = f'data:image/jpeg;base64,{img_base64_str}'
-
-        return html_img_str
-
+        return f'data:image/jpeg;base64,{img_base64_str}'
