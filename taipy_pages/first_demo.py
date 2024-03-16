08860d13efe4c118850560cb8e33712acf784984
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-16 16:25:48 +0100

    style: update first demo

--- taipy_pages/first_demo.py
+++ taipy_pages/first_demo.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 from taipy.gui import Gui
 
-#from taipy_page import page as page1
-from taipy_matplotlib import page as page2
+from taipy_page import page as page1
+#from taipy_matplotlib import page as page2
 #from folium_map import page as page3
 #from taipy_file_page import page as page4
 
@@ -10,8 +10,8 @@ from taipy_matplotlib import page as page2
 upload_folder = Path(__file__).parent.resolve()
 
 gui = Gui()
-#gui.add_page("page1", page1)
-gui.add_page("page2", page2)
+gui.add_page("page1", page1)
+#gui.add_page("page2", page2)
 #gui.add_page("page3", page3)
 #gui.add_page("page4", page4)
 gui.run(run_browser=True, use_reloader=False, upload_folder=upload_folder)
