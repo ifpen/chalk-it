199ac748ef364612f287a2ddbe5c5c8e1cec2b48
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-18 13:44:32 +0100

    new demos

---
+++ taipy_pages/06_file_loader_csv_table.py
@@ -0,0 +1,25 @@
+from taipy.gui import Gui
+import pandas as pd
+# Add the parent directory of `back_end` to sys.path
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+from back_end import *
+
+# Initialize the GUI and set the default DataFrame
+gui = Gui()
+
+df = pd.DataFrame()
+tab_html = ""
+file_path = ""
+
+# Callback function to load the CSV file
+def load_csv(state):
+    print("Load CSV file to + " + state.file_path)
+    state.df = pd.read_csv(state.file_path)
+    state.tab_html = state.df.to_html()
+    print(state.tab_html)
+
+page = ChalkitPage("06_file_loader_csv_table.xprjson")
+gui.add_page("page", page)
+gui.run(run_browser=True, use_reloader=False)
--- taipy_pages/09_pillow_image.py
+++ taipy_pages/09_pillow_image.py
@@ -9,21 +9,26 @@ from PIL import Image, ImageOps
 
 gui = Gui()
 
-# def convert_to_grayscale(image, intensity):
-#     """Convert an image to grayscale with a given intensity."""
-#     if intensity < 0:
-#         intensity = 0
-#     if intensity > 1:
-#         intensity = 1
-#     return ImageOps.grayscale(image).point(lambda x: x * intensity + 255 * (1 - intensity))
+def convert_to_grayscale(image, intensity):
+    """Convert an image to grayscale with a given intensity."""
+    if intensity < 0:
+        intensity = 0
+    if intensity > 1:
+        intensity = 1
+    img_grey = ImageOps.grayscale(image).point(lambda x: x * intensity + 255 * (1 - intensity))
+    return img_grey
 
-# image = Image.open("XN_Fruehjahrswiese_00.jpg")
+image_path = Path(__file__).parent.resolve() / "XN_Fruehjahrswiese_00.jpg"
 
-# greyscale_intensity = 0
+image = Image.open(image_path)
 
-# def on_change(state, var, val):
-#     if (var=="greyscale_intensity"):
-#         state.image = convert_to_grayscale(state.image, val)
+img_grey = convert_to_grayscale(image, 1)
+
+greyscale_intensity = 0
+
+def on_change(state, var, val):
+    if (var=="greyscale_intensity"):
+        state.img_grey = convert_to_grayscale(state.image, val)
 
 page = ChalkitPage("09_pillow_image.xprjson")
 gui.add_page("page", page)
