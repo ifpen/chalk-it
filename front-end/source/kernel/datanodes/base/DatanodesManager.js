00be00550f455ecef5e2e0d4676e20454b5bbd1f
Abir EL FEKI <31621762+abirEF@users.noreply.github.com>
2024-03-20 12:10:13 +0100

    add rename fct for Rest and delay datanodes type
    cherry picked from commit f8c884256c4eab8291186a744ab29504e09964e5

--- front-end/source/kernel/datanodes/base/DatanodesManager.js
+++ front-end/source/kernel/datanodes/base/DatanodesManager.js
@@ -229,6 +229,7 @@ var datanodesManager = (function () {
         swal(`A dataNode with name '${newName}' adready exists.`, 'Please specify a different name', 'error');
         return true;
       }
+
       // handle dependencies
       if (datanodesDependency.hasSuccessors(viewModel.name())) {
         var successors = Array.from(datanodesDependency.getSuccessors(viewModel.name()));
@@ -241,23 +242,41 @@ var datanodesManager = (function () {
           const oldName = viewModel.name();
           const newName = newSettings.settings.name;
           let script = '';
-          if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_plugin') {
+          if (
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_plugin' ||
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_pyodide_plugin'
+          ) {
             script = datanodesManager.getDataNodeByName(successors[prop]).settings().content;
           } else if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'JSON_formula_plugin') {
             script = datanodesManager.getDataNodeByName(successors[prop]).settings().json_var_formula;
+          } else if (
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'REST_web-service_from_datasource'
+          ) {
+            script = datanodesManager.getDataNodeByName(successors[prop]).settings().body;
+          } else if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'JSON_delay_plugin') {
+            script = datanodesManager.getDataNodeByName(successors[prop]).settings().json_input;
           }
           replacedScript = script
             .replace(new RegExp('dataNodes\\["' + oldName + '"\\]', 'g'), 'dataNodes["' + newName + '"]')
             .replace(new RegExp('dataNodes\\.' + oldName, 'g'), 'dataNodes.' + newName);
 
-          datanodesManager.getDataNodeByName(successors[prop]).settings().json_var_formula = replacedScript;
-          if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_plugin') {
+          if (
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_plugin' ||
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'Python_pyodide_plugin'
+          ) {
             datanodesManager.getDataNodeByName(successors[prop]).settings().content = replacedScript;
           } else if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'JSON_formula_plugin') {
             datanodesManager.getDataNodeByName(successors[prop]).settings().json_var_formula = replacedScript;
+          } else if (
+            datanodesManager.getDataNodeByName(successors[prop]).type() === 'REST_web-service_from_datasource'
+          ) {
+            datanodesManager.getDataNodeByName(successors[prop]).settings().body = replacedScript;
+          } else if (datanodesManager.getDataNodeByName(successors[prop]).type() === 'JSON_delay_plugin') {
+            datanodesManager.getDataNodeByName(successors[prop]).settings().json_input = replacedScript;
           }
         }
       }
+
       // handle widget connection
       [bFoundConnection, prop] = isConnectedWithWidgt(viewModel.name());
       if (bFoundConnection) {
