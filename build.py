8e07b0f756c1d4124ffab6f389326c1b245657ce
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-21 09:45:59 +0100

    retoring build default config

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ from dotenv import dotenv_values
 from datetime import datetime
 import json
 
-BUILD_FRONT_END = False
+BUILD_FRONT_END = True
 
 # Set source and destination directories
 src_dir = 'assets/install'
