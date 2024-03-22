2af198de19abedef27a15b862c0f95e0c4334a59
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-22 16:57:27 +0100

    feat: add support for TAIPY_GUI_DESIGN_MODE #214

--- back_end/taipy/chalkit_page.py
+++ back_end/taipy/chalkit_page.py
@@ -14,7 +14,18 @@ from taipy.gui.custom import Page
 from .resource_handler import PureHTMLResourceHandler
 from .chalkit_json_adapter import FunctionJsonAdapter
 
+
 class ChalkitPage(Page):
-    def __init__(self, xprjson_file_name: str = "new_project.xprjson"):
-        super().__init__(resource_handler=PureHTMLResourceHandler(), metadata={"xprjson_file_name": xprjson_file_name})
+
+    def __init__(
+        self,
+        xprjson_file_name: str = "new_project.xprjson",
+        TAIPY_GUI_DESIGN_MODE: bool = False,
+    ):
+        super().__init__(
+            resource_handler=PureHTMLResourceHandler(
+                xprjson_file_name, TAIPY_GUI_DESIGN_MODE
+            ),
+            metadata={"xprjson_file_name": xprjson_file_name},
+        )
         FunctionJsonAdapter().register()
--- back_end/taipy/resource_handler.py
+++ back_end/taipy/resource_handler.py
@@ -14,38 +14,45 @@ from pathlib import Path
 import typing as t
 from flask import send_from_directory, send_file
 from taipy.gui.custom import ResourceHandler
+from ..render import RenderApp
+
 
 class PureHTMLResourceHandler(ResourceHandler):
     id = "htmlresource"
-    run_mode = "editor"
+
+    def __init__(self, xprjson_file_name: str, TAIPY_GUI_DESIGN_MODE: bool):
+        super().__init__()
+        self.xprjson_file_name = xprjson_file_name
+        self.TAIPY_GUI_DESIGN_MODE = TAIPY_GUI_DESIGN_MODE
 
     def get_root_directory(self) -> Path:
         """Dynamically set the root directory based on the existence of 'index.html'."""
         # For develop mode
-        prod_root_dir = (Path(__file__).parent.parent).resolve()
-        
+        prod_root_dir: Path = (Path(__file__).parent.parent).resolve()
+
         if not (prod_root_dir / "index.html").exists():
             # For develop mode
             root_dir = (Path(__file__).parent / ".." / ".." / "front-end").resolve()
         else:
             # For production mode
             root_dir = prod_root_dir
-        
+
         return root_dir
 
     def get_resources(self, path: str, base_bundle_path: str) -> t.Any:
         """Get the resources from the specified path."""
-        root_dir = self.get_root_directory()  # Set root_dir dynamically
-        
-        if not path or path == "index.html" or "." not in path:
-            return send_from_directory(root_dir, "index.html")
-        
-		# Serve the requested file if it exists
-        file_path = root_dir / path
-        if file_path.is_file():
-            return send_from_directory(root_dir, path)
-        
-        # if "taipy-gui-base.js" in path:
-        #     return send_file(base_bundle_path)
+        root_dir: Path = self.get_root_directory()  # Set root_dir dynamically
+        if self.TAIPY_GUI_DESIGN_MODE:
+            if not path or path == "index.html" or "." not in path:
+                return send_from_directory(root_dir, "index.html")
+
+            # Serve the requested file if it exists
+            file_path: Path = root_dir / path
+            if file_path.is_file():
+                return send_from_directory(root_dir, path)
+        else:
+            xprjson_path: Path = (Path.cwd() / self.xprjson_file_name).resolve()
+            if xprjson_path.is_file():
+                return RenderApp.start_runtime(root_dir, xprjson_path)
 
         return ("File not found", 404)
