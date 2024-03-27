efa0cb6271744c00710d22695b6625875758e7ac
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-27 22:12:00 +0100

    feat: enable discover module #220

--- front-end/source/angular/modules/sidebar/sidebar.controller.js
+++ front-end/source/angular/modules/sidebar/sidebar.controller.js
@@ -177,13 +177,6 @@ angular.module('modules.sidebar').controller('SidebarController', [
       xdash.openFile('project', 'local');
     };
 
-    /*---------- Open Chalk'it documentation ----------------*/
-    $scope.openDoc = function () {
-      const docURL = xDashConfig.urlDoc;
-      const tab = window.open(docURL, '_blank');
-      tab.focus();
-    };
-
     /*---------- start Taipy project ----------------*/
     $scope.startTaipyProject = function () {
       ManagePrjService.clearForNewProject();
