4fc5e6b87c1b5fee65963df2d6d3a09c23e4c186
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-28 17:18:53 +0100

    maps doc work. to be contd

--- documentation/taipy_docs/wdg/maps/osm-heatmap-view.py
+++ documentation/taipy_docs/wdg/maps/osm-heatmap-view.py
@@ -7,24 +7,18 @@ CH4_df = pd.read_csv("CH4_data.csv")
 
 CH4_data = CH4_df.to_dict(orient='records')
 
-CH4_heatmap_config = {
-  "opacity": 0.8,
-  "radius": 4.4,
-  "disableAutoscale": False,
-  "min": 0,
-  "max": 4,
-  "colorScale": "interpolateSpectral",
-  "reverseColorScale": False
-}
-
 CH4_heatmap = {
   "data" : CH4_data,
-  "config" : CH4_heatmap_config
+  "config" : {
+    "opacity": 0.8,
+    "radius": 4.4,
+    "disableAutoscale": False,
+    "min": 0,
+    "max": 4,
+    "colorScale": "interpolateSpectral",
+    "reverseColorScale": False
+  }
 }
-
-def on_change(state, var, val):
-  if (var == "CH4_heatmap"):
-    state.CH4_heatmap = val
 
 gui = Gui()
 page = ChalkitPage("osm-heatmap-view_modif.xprjson")
