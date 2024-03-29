b7edcdd22144f0587dcfc76f7cdba40e59d46a0a
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-03-29 17:26:51 +0100

    style: cleanup first_demo

--- taipy_pages/first_demo.py
+++ taipy_pages/first_demo.py
@@ -4,7 +4,6 @@ from taipy.gui import Gui
 from taipy_page import page as page1
 from taipy_matplotlib import page as page2
 from folium_map import page as page3
-from taipy_file_page import page as page4
 
 # Define the path for the upload folder relative to the current script's directory
 upload_folder = Path(__file__).parent.resolve()
@@ -14,5 +13,4 @@ gui = Gui()
 gui.add_page("page1", page1)
 gui.add_page("page2", page2)
 gui.add_page("page3", page3)
-gui.add_page("page4", page4)
 gui.run(run_browser=True, use_reloader=False, upload_folder=upload_folder)
