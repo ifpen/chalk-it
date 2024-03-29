b6e4b7ddfab7b58adddb0103d1ce63fd9ace056a
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-29 16:08:06 +0100

    fix select tuto

--- documentation/taipy_docs/training/11_select/11_select.py
+++ documentation/taipy_docs/training/11_select/11_select.py
@@ -3,8 +3,8 @@ from chlkt import *
 
 gui = Gui()
 
-def print_message(select_fruits):
-    message = "The selected fruit is: " + ", ".join(select_fruits)
+def print_message(select_fruit):
+    message = "The selected fruit is: " + select_fruit
     return message
 
 fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "kiwi", "lemon", "mango"]
@@ -12,7 +12,7 @@ select_fruit = fruits[0]
 message = print_message(select_fruit)
 
 def on_change(state, var, val):
-    if (var == "select_fruits"):
+    if (var == "select_fruit"):
         state.message = print_message(state.select_fruit)
 
 
