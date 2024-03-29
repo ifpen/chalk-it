43f766c7becace5ea9428158a2019a435ef1692c
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-29 15:14:35 +0100

    annotations for taipy. to be contd

---
+++ documentation/taipy_docs/wdg/annotation/video_screenshot.py
@@ -0,0 +1,13 @@
+from taipy.gui import Gui
+from chlkt import *
+
+imageData = {
+  "b64":"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABjElEQVR42mNk",
+  "mimeType":"image/png",
+  "imageData":"iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABjElEQVR42mNk"
+}
+
+gui = Gui()
+page = ChalkitPage("video_screenshot.xprjson")
+gui.add_page("page", page)
+gui.run(run_browser=True, use_reloader=False)
