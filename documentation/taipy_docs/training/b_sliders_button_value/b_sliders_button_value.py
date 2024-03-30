97e262921237da94d885c3196b800a50ff76bbb7
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-03-30 22:42:59 +0100

    applying python module naming conventions to training examples

---
+++ documentation/taipy_docs/training/b_sliders_button_value/b_sliders_button_value.py
@@ -0,0 +1,16 @@
+from taipy.gui import Gui
+from chlkt import *
+
+gui = Gui()
+
+a = 1
+b = 5
+c = a + b
+
+def compute_addition(state, var, val):
+        state.c = state.a + state.b
+
+
+page = ChalkitPage("b_sliders_button_value.xprjson")
+gui.add_page("page", page)
+gui.run(run_browser=True, use_reloader=False)
