57b37666eb88826405e99295bf4468cfaaa66af4
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-04-01 16:55:19 +0200

    fixes

--- documentation/taipy_docs/wdg/date-time/date_picker.py
+++ documentation/taipy_docs/wdg/date-time/date_picker.py
@@ -1,6 +1,9 @@
 from taipy.gui import Gui
 from chlkt import *
 
+
+selected_date = "2024-04-01"
+
 gui = Gui()
 page = ChalkitPage("date_picker.xprjson", designer_mode=True)
 gui.add_page("page", page)
