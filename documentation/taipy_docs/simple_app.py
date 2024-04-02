ee1e48b83439f881ea095c65d30dd7348ed6cd3c
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-04-02 10:30:04 +0200

    fix first examples

--- documentation/taipy_docs/simple_app.py
+++ documentation/taipy_docs/simple_app.py
@@ -1,11 +1,15 @@
 from taipy.gui import Gui
-from chlkt import *
+from taipy_designer import *
 
 a = 1
 b = 5
 c = a + b
 
-page = ChalkitPage("sliders_value.xprjson", designer_mode=True)
+def on_change(state, var, val):
+    if (var == "a" or var == "b"):
+        state.c = state.a + state.b
+
+page = DesignerPage("sliders_value.xprjson", designer_mode=True)
 
 gui = Gui()
 gui.add_page("page", page)
