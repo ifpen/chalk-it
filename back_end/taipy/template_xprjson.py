25a4aa06f30a63e9cb4b2b80b951975f8c4ff0c8
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-04-30 14:58:21 +0200

    style: format template_xprjson.py

--- back_end/taipy/template_xprjson.py
+++ back_end/taipy/template_xprjson.py
@@ -11,64 +11,52 @@
 
 
 xprjson_template = {
-	"meta": {
-		"version": "2.992.8836",
-		"date": "",
-		"name": "",
-		"description": "",
-		"groupName": "",
-		"tags": [],
-		"schedulerLogOff": True
-	},
-	"data": {
-		"datanodes": []
-	},
-	"libraries": {
-		"pyodideStandard": [],
-		"pyodideMicropip": []
-	},
-	"scaling": {
-		"widthPx": 1294,
-		"heightPx": 590,
-		"scrollWidthPx": 1294,
-		"scrollHeightPx": 590,
-		"widthVw": 84.68586387434554,
-		"heightVh": 78.66666666666667,
-		"scrollWidthVw": 84.68586387434554,
-		"scrollHeightVh": 78.66666666666667,
-		"scalingMethod": "scaleTwh",
-		"media": "large",
-		"colDims": {
-			"widthPx": 1294.4,
-			"heightPx": 590.4,
-			"widthVw": 84.71204188481677,
-			"heightVh": 78.72,
-			"rowHeightPercent": 100
-		}
-	},
-	"device": {
-		"cols": {
-			"valueRow": "1",
-			"valueCol": "1",
-			"maxCells": 1,
-			"maxCols": 1,
-			"classType": "col-md-12"
-		},
-		"droppers": {
-			"dpr1": {}
-		},
-		"backgroundColor": "",
-		"theme": "default"
-	},
-	"dashboard": {},
-	"connections": {},
-	"exportOptions": "projectToTargetWindow",
-	"pages": {
-		"pageNames": [
-			"Page 1"
-		],
-		"defaultPage": {}
-	},
-	"checkExportOptions": True,
-	"navBarNotification": False
+    "meta": {
+        "version": "2.992.8836",
+        "date": "",
+        "name": "",
+        "description": "",
+        "groupName": "",
+        "tags": [],
+        "schedulerLogOff": True,
+    },
+    "data": {"datanodes": []},
+    "libraries": {"pyodideStandard": [], "pyodideMicropip": []},
+    "scaling": {
+        "widthPx": 1294,
+        "heightPx": 590,
+        "scrollWidthPx": 1294,
+        "scrollHeightPx": 590,
+        "widthVw": 84.68586387434554,
+        "heightVh": 78.66666666666667,
+        "scrollWidthVw": 84.68586387434554,
+        "scrollHeightVh": 78.66666666666667,
+        "scalingMethod": "scaleTwh",
+        "media": "large",
+        "colDims": {
+            "widthPx": 1294.4,
+            "heightPx": 590.4,
+            "widthVw": 84.71204188481677,
+            "heightVh": 78.72,
+            "rowHeightPercent": 100,
+        },
+    },
+    "device": {
+        "cols": {
+            "valueRow": "1",
+            "valueCol": "1",
+            "maxCells": 1,
+            "maxCols": 1,
+            "classType": "col-md-12",
+        },
+        "droppers": {"dpr1": {}},
+        "backgroundColor": "",
+        "theme": "default",
+    },
+    "dashboard": {},
+    "connections": {},
+    "exportOptions": "projectToTargetWindow",
+    "pages": {"pageNames": ["Page 1"], "defaultPage": {}},
+    "checkExportOptions": True,
+    "navBarNotification": False,
 }
