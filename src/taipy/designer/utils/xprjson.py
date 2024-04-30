bc1a3be00cfc44561fea653029bb9aac682284a1
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-04-30 17:30:18 +0200

    feat: set iso date for the default xprjson

--- src/taipy/designer/utils/xprjson.py
+++ src/taipy/designer/utils/xprjson.py
@@ -6,17 +6,12 @@ from datetime import datetime
 import pytz
 
 
-def _getDateTime():
+def _get_date_time():
     # Choose the timezone
     timezone = pytz.timezone("Europe/Paris")
-    # Get the current date and time in the specified timezone
     current_time = datetime.now(timezone)
-    # Format the date and time
-    formatted_time = current_time.strftime("%a %b %d %Y %H:%M:%S GMT%z")
-    # Adjust the format to include a colon in the timezone part
-    formatted_time_with_colon = formatted_time[:-2] + ":" + formatted_time[-2:]
-
-    return formatted_time_with_colon
+    # Return the date and time in ISO 8601 format
+    return current_time.isoformat()
 
 
 def _remove_extension(filename, extension=".xprjson"):
@@ -26,7 +21,7 @@ def _remove_extension(filename, extension=".xprjson"):
 
 
 def update_xprjson(xprjson, name):
-    xprjson["meta"]["date"] = _getDateTime()
+    xprjson["meta"]["date"] = _get_date_time()
     xprjson["meta"]["name"] = _remove_extension(name)
 
     return xprjson
