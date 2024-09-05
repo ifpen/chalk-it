c0acecde9ba593fd9e09ac2783c238108e640512
GhilesHideur <47829666+GhilesHideur@users.noreply.github.com>
2024-09-05 13:15:13 +0200

    feat: update eslintrc

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -6,11 +6,12 @@ module.exports = {
   ignorePatterns: ['thirdparty/**'],
   extends: 'eslint:recommended',
   parserOptions: {
+    ecmaVersion: 2022,
     sourceType: 'module',
   },
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'unix'],
+    // 'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
     semi: ['error', 'always'],
   },
