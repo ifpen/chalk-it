b80c3f94f18731224a06da2fd52c111e8c060296
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-09-13 14:53:26 +0200

    making script more robust

--- e2e/src/check-window-size.ts
+++ e2e/src/check-window-size.ts
@@ -1,13 +1,18 @@
 import { Builder } from 'selenium-webdriver';
+import * as chrome from 'selenium-webdriver/chrome.js';
 
 (async () => {
   async function checkWindowSize() {
-      const driver = await new Builder().forBrowser('chrome').build();
+      const options = new chrome.Options();
+      options.addArguments('--headless');
+      options.addArguments('--disable-gpu');
+      options.addArguments('--no-sandbox');
+      options.addArguments('--disable-dev-shm-usage');
+
+      const driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build();
 
       try {
-          // Get the window size
           const windowSize = await driver.manage().window().getRect();
-
           console.log(`Current window size: width=${windowSize.width}, height=${windowSize.height}`);
       } finally {
           await driver.quit();
