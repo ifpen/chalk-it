47c665c159ae9efb57e79f86b435585ca2e0a06e
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-09-13 14:42:40 +0200

    hopefully found fix

--- e2e/src/check-window-size.ts
+++ e2e/src/check-window-size.ts
@@ -1,17 +1,18 @@
 import { Builder } from 'selenium-webdriver';
-import 'chromedriver';
 
-async function checkWindowSize() {
-    const driver = await new Builder().forBrowser('chrome').build();
+(async () => {
+  async function checkWindowSize() {
+      const driver = await new Builder().forBrowser('chrome').build();
 
-    try {
-        // Get the window size
-        const windowSize = await driver.manage().window().getRect();
+      try {
+          // Get the window size
+          const windowSize = await driver.manage().window().getRect();
 
-        console.log(`Current window size: width=${windowSize.width}, height=${windowSize.height}`);
-    } finally {
-        await driver.quit();
-    }
-}
+          console.log(`Current window size: width=${windowSize.width}, height=${windowSize.height}`);
+      } finally {
+          await driver.quit();
+      }
+  }
 
-checkWindowSize();
+  await checkWindowSize();
+})();
