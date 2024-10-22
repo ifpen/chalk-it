89d95958f69e6506293178912447b1dd6d2e1f63
Mongi Ben Gaid <39761578+bengaid@users.noreply.github.com>
2024-10-22 20:34:28 +0200

    added material ui example

--- front-end/source/kernel/dashboard/plugins/annotation-scoring/MyComponent.jsx
+++ front-end/source/kernel/dashboard/plugins/annotation-scoring/MyComponent.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Slider from '@mui/material/Slider';
+import Typography from '@mui/material/Typography';
 
 const MyComponent = () => {
-  return <h1>Hello from React!</h1>;
-};
+  const [value, setValue] = useState(30);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <div style={{ width: 300, margin: '50px auto' }}>
+      <Typography gutterBottom>Value: {value}</Typography>
+      <Slider value={value} onChange={handleChange} aria-labelledby="continuous-slider" />
+    </div>
+  );};
 
 export default MyComponent;
